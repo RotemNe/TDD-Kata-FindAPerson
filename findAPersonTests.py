fc5c5d848d94ff9b52ad7c53f74b113f7a5ddb6e
Rotem <rotemmm0@gmail.com>
2015-05-12 09:40:22 +0300

    GREEN another test for missing person

--- findAPersonTests.py
+++ findAPersonTests.py
@@ -8,16 +8,15 @@ from crowdMap import Crowdmap
 
 class FindAPersonTestes(unittest.TestCase):
 	def setUp(self):
-		self.crowdmap=Crowdmap()
+		self.crowdmap=Crowdmap( ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"])
 
  	def test_getAllPostsForName(self):
  		posts=self.crowdmap.get_all_posts_for("Or")
- 		self.assertIn("Or",posts)
+ 		self.assertEquals( ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"],posts)
 
  	def test_getAllPostsForMissingName(self):
- 		posts=self.crowdmap.get_all_posts_for("Or1")
- 		self.assertNotIn("Or2",posts)
-
+ 		posts=self.crowdmap.get_all_posts_for("Joe")
+ 		self.assertEquals( [],posts)
 
 
 if __name__ == '__main__':
