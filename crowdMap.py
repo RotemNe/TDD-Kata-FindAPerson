49978c13a8a9062fa45dc80b47317e08dcc90140
Rotem <rotemmm0@gmail.com>
2015-05-19 09:27:28 +0300

    RED:is_location_for_name use empty location list and return false

--- crowdMap.py
+++ crowdMap.py
@@ -1,4 +1,5 @@
 
+from locationService import locationService
 
 class Crowdmap(object):
 	def __init__(self,init_list):
@@ -7,5 +8,11 @@ class Crowdmap(object):
  	def get_all_posts_for(self,name):
  		 return [post for post in self.list if post.find(name)!=-1]
 
- 	def is_location_for_name(self,name):
- 		return True
+ 	def is_location_for_name(self,name):	
+ 		locations=[]
+ 		posts= self.get_all_posts_for(name)
+ 		for post in posts:
+ 			for loaction in locations:
+ 				if location in post:
+ 					return True
+ 		return False
